40b662a92a3b66506b7a982cec5ea2d5e37c4007
CheolHwi <sunjin1233@naver.com>
2017-12-08 16:41:07 +0900

    진짜1차

---
+++ app/src/main/java/com/example/cheolhwi/loginandregistration/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.cheolhwi.loginandregistration.app;
+
+/**
+ * Created by CheolHwi on 2017-12-02.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://203.252.53.190/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://203.252.53.190/android_login_api/register.php";
+}
